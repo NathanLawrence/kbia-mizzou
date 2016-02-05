84c7bb88f30fc023b0846c26c577874fd942390f
Nathan Lawrence <lawrencen@missouri.edu>
2016-02-05 14:07:50 -0600

    Glyphicons are now a fixed thing!

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,17 @@ gulp.task('test', function(){
 	gutil.log('This is a test.');
 });
 
-gulp.task('less', function(){
+gulp.task('img', function(){
+	return gulp.src('components/img/*')
+	.pipe(gulp.dest('build/img'));
+});
+
+gulp.task('fonts', function(){
+	return gulp.src('components/fonts/*')
+	.pipe(gulp.dest('build/fonts'));
+});
+
+gulp.task('less', ['fonts'], function(){
 	return gulp.src('components/less/styles.less')
     .pipe(less({}))
     .pipe(gulp.dest('build/css'));
@@ -17,9 +27,4 @@ gulp.task('less', function(){
 gulp.task('html', function(){
 	return gulp.src('components/**/*.html')
 	.pipe(gulp.dest('build'));
-});
-
-gulp.task('img', function(){
-	return gulp.src('components/img/*')
-	.pipe(gulp.dest('build/img'));
 });
